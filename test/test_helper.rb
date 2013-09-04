f07f21ab2511cc83b2c1b4b9673bf8b2b33a18bb
Rob Race <rob.race@me.com>
2013-09-03 21:55:54 -0700

    Code coverage adjustment

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -4,7 +4,7 @@ gem 'minitest'
 require 'minitest/spec'
 require 'minitest/autorun'
 
+require File.expand_path('../../lib/math_probability.rb', __FILE__)
+
 require 'coveralls'
 Coveralls.wear!
-
-require File.expand_path('../../lib/math_probability.rb', __FILE__)
